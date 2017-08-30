1780d8d310c1f69efc07e76decb3b613d8eb48a5
amvinoth143 <30728434+amvinoth143@users.noreply.github.com>
2017-08-30 20:16:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,7 +32,7 @@ submit.onclick = function (){
   var list = '';
   for (var i=0;i<names.length;i++)
   {
-      list += '<li>' + names[i] + '/li';
+      list += '<li>' + names[i];
   }
   var ul = document.getElementById('namelist');
   ul.innerHTML = list;

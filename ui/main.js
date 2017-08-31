fe26dbee3f5cbba0821550b7e1808871e982b0de
amvinoth143 <30728434+amvinoth143@users.noreply.github.com>
2017-08-31 19:44:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,11 +29,4 @@ button.onclick = function(){
   request.send(null);
 };
 
-//Submit Name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
-submit.onclick = function (){
-  //make a request to the server and send the name
 
-};

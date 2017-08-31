0f0c0b50a1ec8d4c71dbab97db7c8c183ee929e1
amvinoth143 <30728434+amvinoth143@users.noreply.github.com>
2017-08-31 19:30:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,14 +85,14 @@ app.get('/counter', function(req,res){
  res.send(counter.toString());
 });
 
-var names = [];
-app.get('/submit-name', function (req, res) {
+var names = []; 
+app.get('/submit-name/:name', function (req, res) {
 //Get the name from the request
-var name = req.query.name;
+var name = req.params.name;
 
 names.push(name);
 //JSON Java Script Object Notation
-res.send(JSON.stringify(name));
+res.send(JSON.stringify(names));
 });
 
 app.get('/:articleName', function (req, res) {
